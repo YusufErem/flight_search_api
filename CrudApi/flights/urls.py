7db879b7c14c4ccfebc27b546161278eaedc14f4
Yusuf <ysferem@gmail.com>
2024-05-15 14:51:00 +0300

    search_airpor and search_flight has created with city parameter

--- CrudApi/flights/urls.py
+++ CrudApi/flights/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [  
     path('',views.create),
-    path('search/',views.search),
+    path('search_flight/',views.search_flight),
+    path('search_airport/',views.search_airport),
     path('update/',views.update),
     path('delete/',views.delete),
 
