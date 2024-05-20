d60c0ac61e25a9551fcb33a62380579b5929d55c
Yusuf <ysferem@gmail.com>
2024-05-20 17:05:28 +0300

    Crud operations has done

--- CrudApi/flights/models.py
+++ CrudApi/flights/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Havaalanlari(models.Model):
     sehir = models.CharField()
+    
+    def __str__(self):
+        return f"Sehir:{self.sehir}"
 
 class Ucuslar(models.Model):
     kalkis_havaalani = models.ForeignKey(Havaalanlari,max_length=100, on_delete= models.CASCADE,related_name = 'kalkis_havaalanlari')
@@ -11,3 +14,6 @@ class Ucuslar(models.Model):
     kalkis_zamani = models.DateTimeField()
     donus_zamani = models.DateTimeField()
     fiyat = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.kalkis_havaalani, self.varis_havaalani,self.kalkis_zamani,self.donus_zamani,self.fiyat}"
--- CrudApi/flights/views.py
+++ CrudApi/flights/views.py
@@ -1,55 +1,75 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.http import JsonResponse
-from django.shortcuts import render
 from flights.serializer import HavaalanlariSerializer, UcuslarSerializer
 from flights.models import Havaalanlari, Ucuslar
-from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework import status,viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
 # Create your views here.
-@api_view(['POST'])
-def airport_create(req):
-    serialize = HavaalanlariSerializer(data = req.data)
-    if serialize.is_valid():
-        serialize.save()
-        return Response(serialize.data)
-    else:
-        return Response(serialize.errors)
+class HavaalaniViewset(viewsets.ModelViewSet):
+    serializer_class = HavaalanlariSerializer
 
-@api_view(['POST'])
-def flight_create(req):
-    pass
-
-@api_view(['GET'])
-def search_airport(req):
-    
-    Havaalanlari_query = req.query_params.get('havaalani').capitalize()
-
-    if Havaalanlari_query:
-        queryset = Havaalanlari.objects.filter(sehir = Havaalanlari_query)
-        serializer = HavaalanlariSerializer(queryset,many= True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
+    def get_queryset(self):
         queryset = Havaalanlari.objects.all()
-        serializer = HavaalanlariSerializer(queryset,many= True)
-        return Response(serializer.data)
+        city = self.request.GET.get('sehir')
+        if city: 
+            queryset = queryset.filter(sehir__icontains = city)
+        return queryset
+    
+class UcuslarViewset(viewsets.ModelViewSet):
+    serializer_class = UcuslarSerializer
+    queryset = Ucuslar.objects.all()
+    
+    def list(self, request):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def create(self, request):
+        print("Error ver")
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    @action(detail=False, methods=['get'], url_path='list')
+    def list_ucus(self,request):
+        queryset = Ucuslar.objects.all()
+        kalkis_yeri = self.request.GET.get('kalkis-yeri')
+        varis_yeri = self.request.GET.get('varis-yeri')
+        kalkis_tarihi = self.request.GET.get('kalkis-tarihi')
+        donus_tarihi = self.request.GET.get('donus-tarihi')
 
+        if kalkis_yeri:
+            queryset = queryset.filter(kalkis_havaalani__sehir__icontains=kalkis_yeri)
+        if varis_yeri:
+            queryset = queryset.filter(varis_havaalani__sehir__icontains=varis_yeri)
+        if kalkis_tarihi:
+            queryset = queryset.filter(kalkis_zamani__date__gte=kalkis_tarihi)
+        if donus_tarihi:
+            queryset = queryset.filter(donus_zamani__date__lte=donus_tarihi)
 
-@api_view(['GET'])
-def search_flight(request):
-    kalkis_havaalani1 = request.query_params.get('kalkis_havaalani')
-    if kalkis_havaalani1:
-        havaalanii =get_object_or_404(Havaalanlari,sehir=kalkis_havaalani1)
-        queryset = Ucuslar.objects.filter(kalkis_havaalani= havaalanii)
-        serializer = UcuslarSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        return Response({'error': 'Kalkış havaalanı belirtilmedi'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(queryset, many=True)
+        return  Response(serializer.data)
 
+    @action(detail=False, methods=['post'], url_path='create')
+    def create_ucus(self, request):
+        kalkis_yeri = request.data.get('kalkis_havaalani')
+        inis_yeri = request.data.get('varis_havaalani')
+        kalkis_zamani = request.data.get('kalkis_zamani')
+        donus_zamani = request.data.get('donus_zamani')
+        
+        if kalkis_yeri == inis_yeri:
+            return Response(
+                    {"detail": "Kalkis Havaalani ile Varis Havaalani Ayni Sehirde Olamaz."},
+                    status=status.HTTP_400_BAD_REQUEST)
 
-def update(req):
-    pass
+           
+        if kalkis_zamani and donus_zamani:
+            print(f"{kalkis_yeri,kalkis_zamani,donus_zamani,inis_yeri}TEST")
 
-def delete(req):
-    pass
+            if kalkis_zamani >= donus_zamani:
+                return Response(
+                    {"detail": "Kalkış tarihi dönüş tarihinden büyük veya eşit olamaz."},
+                    status=status.HTTP_400_BAD_REQUEST)
+            
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
